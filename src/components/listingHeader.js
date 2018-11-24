364feeed1ab61494f88e8cf7ee99f856cf0597b1
ahmedreda89 <e.ahmedreda@gmail.com>
2018-11-25 01:42:20 +0200

    feat: components and data

---
+++ src/components/listingHeader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class listingHeader extends React.Component {
+    render(){
+        return(
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+        );
+    }
+}
